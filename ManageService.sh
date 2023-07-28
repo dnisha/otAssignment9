c1f27020a3e029069365e4af01498a5a81226b0d
Aditya <adityadhoundiyal1999@gmail.com>
2023-07-28 15:40:05 +0530

    Manage sevice implemented by Aditya

---
+++ ManageService.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+service_name=$2
+operation=$3
+
+if [ "$2" == "daemon-reload" ]
+then 
+
+systemctl daemon-reload
+echo "System daemon reloaded."
+
+else
+
+
+
+  case $3 in
+     'start' )
+         systemctl start $service_name
+         echo "Service '$service_name' started."   
+         ;;
+
+     "restart" )
+         systemctl restart $service_name
+         echo "Service '$service_name' restarted."
+        ;;  
+
+     "stop" )
+         systemctl stop $service_name
+         echo "Service '$service_name' stopped."    
+        ;;   
+  esac
+   
+fi 
